10c3cd81b5c825302db5e8dc39ef033e743e9654
Maxime BUS <bus.maxime@mailoo.org>
2017-04-12 21:55:48 +0200

    Add command line interface and cities/names database

--- generation-mot/Main.hs
+++ generation-mot/Main.hs
@@ -8,23 +8,8 @@ import System.IO
 import System.Random
 import System.Environment
 
-dictionary = [ "maxime"
-             , "roxanne"
-             , "zoé"
-             , "nathalie"
-             , "mandy"
-             , "inès"
-             , "loan"
-             , "guillaume"
-             , "johan"
-             , "marvin"
-             , "emmanuel"
-             , "mathieu"
-             , "antoine"
-             ]
- 
 computeWord :: Int -> Distribution -> IO String
-computeWord length dist@(Distribution order d) = computeWord' length (take order $ repeat '^') dist 
+computeWord length dist@(Distribution order d) = fmap (drop order) $ computeWord' length (take order $ repeat '^') dist 
 
 computeWord' :: Int -> String -> Distribution -> IO String
 computeWord' 0 w _ = return w
@@ -33,10 +18,6 @@ computeWord' l w dist@(Distribution o d) = do
         potentialLetters = Map.toList $ Map.filterWithKey (\ k _ -> init k == endOfWord) d
         sorted           = sortBy (\ c p -> if (snd c) > (snd p) then GT else LT) potentialLetters
         total            = foldr (\ v a -> a + (snd v)) 0 sorted
-
-    --putStrLn $ "potential letter : " ++ show potentialLetters
-    --putStrLn $ "current word : " ++ show w
-    --putStrLn $ "computed end of word : " ++ show endOfWord
 
     letter <- computeLetter 0 total sorted
 
@@ -48,36 +29,40 @@ computeLetter prev total (d:dist) = do
     
     rnd <- randomRIO (0, 1) :: IO Float
 
-    -- putStrLn $ "prev value is : " ++ show prev
-    -- putStrLn $ "random number is : " ++ show rnd
-
     let curr  = snd d  
         cumul = prev + curr
         rnd'  = floor (rnd * fromIntegral(total))
-
-    -- putStrLn $ "cumul is : " ++ show cumul
-    -- putStrLn $ "random number prime is : " ++ show rnd'
 
     if rnd' < cumul 
     then return $ [last . fst $ d]
     else computeLetter cumul total dist
 
 main = do
-    args <- getArgs
-    
-    let wordLength = read $ args !! 0 :: Int
-        order      = read $ args !! 1 :: Int
-	file       = args !! 2
-    handle <- openFile file ReadMode
+    (command:args) <- getArgs
+
+    let (Just action) = lookup command dispatch
+
+    action args
+
+-- Code based on http://learnyouahaskell.com/input-and-output#command-line-arguments 
+dispatch :: [(String, [String] -> IO ())]
+dispatch = [ ("generateDistribution", generateDistributionFile)
+           , ("generateWord", generateWord)
+           ]
+
+generateDistributionFile :: [String] -> IO ()
+generateDistributionFile [path, order] = do
+    handle <- openFile path ReadMode
     contents <- hGetContents handle
-    
-    let distribution = computeDistribution order (lines contents)
 
-    toFile distribution "distribution_prenoms.txt"
-    --distribution <- fromFile "distribution_prenoms.txt"
+    let distribution = computeDistribution (read order) (lines contents)
 
-    --let distribution = computeDistribution 2 dictionary
+    toFile distribution (path ++ ".dist")
 
-    word <- computeWord (read (args !! 0)) distribution
+generateWord :: [String] -> IO ()
+generateWord [distributionPath, wordLength] = do
+    distribution <- fromFile distributionPath
+
+    word <- computeWord (read wordLength) distribution
 
     putStrLn word
